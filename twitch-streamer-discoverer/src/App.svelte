28627c35d5f021635ed73a53c19097ce707dff8d
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-11-21 10:25:22 +0000

    init svelte typescript tailwind vite

---
+++ twitch-streamer-discoverer/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TailwindCss from './TailwindCSS.svelte'
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+  const a = () => {console.log('a')
+  }
+</script>
+
+<main>
+</main>
+
+<style>
+</style>
